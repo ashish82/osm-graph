d5923ff11e5edcc42d3d25dc2f0973daefe8f699
Ashish Srivastava <Ashish.Srivastava@go-mmt.com>
2018-09-03 19:35:25 +0530

    update db details

--- src/main/java/com/mycompany/app/ServiceConstants.java
+++ src/main/java/com/mycompany/app/ServiceConstants.java
@@ -2,11 +2,11 @@ package com.mycompany.app;
 
 public class ServiceConstants {
 
-	public static String DB_FOLDER = "/data/databases/mapdata";
+	public static String DB_FOLDER = "/data/databases/evstation";
 	public static String DB_USERNAME = "admin";
 	public static String DB_PASSWORD = "admin";
-	//public static String DB_URL = "remote:nitigraph.centralindia.cloudapp.azure.com";
-	public static String DB_URL = "remote:localhost";
+	public static String DB_URL = "remote:newdelhi.centralindia.cloudapp.azure.com";
+	//public static String DB_URL = "remote:localhost";
 	public static String RESET_COMMAND = "resetdb";
 	public static String LOCATION_CLASS = "Location";
 	public static String ROAD_CLASS = "Road";
